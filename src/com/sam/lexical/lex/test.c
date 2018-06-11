2d7732ef5dec1a8c4fd691c67111ab1dc84b4a07
shenjiahao <1843781563@qq.com>
2018-06-11 18:57:46 +0800

    Add Flex.

---
+++ src/com/sam/lexical/lex/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    for (a = 0; a < 5; ++a) {
+        if (a % 2 == 0)
+            continue;
+        printf("Hello %d", a);
+    }
+    if (true) {
+        char b = 'b';
+    }
+    return 0;
+}
